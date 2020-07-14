9804e1edf0a75ada68ebc9d44e3ca997c1c4a8c0
sheikhsultan <sheikhsultanm@gmail.com>
2020-07-14 12:40:34 +0600

    forLoop complete

--- for-loop.js
+++ for-loop.js
@@ -6,3 +6,17 @@ for(var i = 0; i < 10; i++){
 for(var i = 0; i <= 10; i++){
     console.log(i);
 }
+
+
+var nums = [55, 66, 77, 88, 99, 22, 11, 44];
+
+for(var i = 0; i < nums.length; i++){
+    var element = nums[i];
+    console.log(element);
+}
+
+var friends = ['redu', 'fuckK', 'anik', 'abdullah', 'arif', 'mithu', 'mmishad'];
+for(var i = 0; i < friends.length; i++){
+    var element = friends[i];
+    console.log( element);
+}
