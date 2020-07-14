93abc1bcb3a39b85edaf4a19787a6393e17dca98
sheikhsultan <sheikhsultanm@gmail.com>
2020-07-14 08:04:14 +0600

    added lesson 13-2 and lesson 13-3. lesson 13-3 starts from now

---
+++ 13-2_array.js
@@ -0,0 +1,35 @@
+// array er position shuru hoy "0" theke
+
+// ai gulake oneke collection ba list bole. kintu shohoj bhabe aitake bole array
+
+var friendsAge = [15, 17, 14, 16];
+console.log(friendsAge);
+
+var friendsAge = [15, 17, 14, 16];
+console.log(friendsAge [0]);
+
+var friendsAge = [15, 17, 14, 16];
+console.log(friendsAge [3]);
+
+
+var friendsAge = [15, 17, 14, 16];
+var sonaliAge = friendsAge[2];
+console.log(sonaliAge);
+
+var friendsAge = [15, 17, 14, 16];
+var sonaliAge = friendsAge[2];
+friendsAge[1] = 21;
+console.log(friendsAge);
+
+var friendsAge = [15, 17, 14, 16];
+var sonaliAge = friendsAge[2];
+friendsAge[1] = 21;
+var position = friendsAge.indexOf(14);
+console.log(position);
+
+var friendsAge = [15, 17, 14, 16];
+var sonaliAge = friendsAge[2];
+friendsAge[1] = 21;
+var position = friendsAge.indexOf(141);
+console.log(position);
+
---
+++ 13-3_array.js
@@ -0,0 +1,13 @@
+13-3
+
+// how to add in array
+
+var friendsAge = [15, 17, 14, 16];
+var sonaliAge = friendsAge[2];
+friendsAge[1] = 21;
+var position = friendsAge.indexOf(141);
+// when we will add some thing in array, ww'll use .push after the 'array name'
+friendsAge.push(15);
+friendsAge.push(25);
+friendsAge.push(28);
+console.log(friendsAge);
