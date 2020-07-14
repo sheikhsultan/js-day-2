c8986d78d5d68a1a6d164c59b79e752d20cf533c
sheikhsultan <sheikhsultanm@gmail.com>
2020-07-14 11:59:21 +0600

    while lesson complete and for loop started

---
+++ while.js
@@ -0,0 +1,10 @@
+var num = 10;
+while (num < 15){
+    console.log(num);
+    num++; // we can also write it like this || num = num + 1; ||
+}
+var num = 10;
+while (num <= 15){
+    console.log(num);
+    num++; // we can also write it like this || num = num + 1; ||
+}
